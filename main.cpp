3e08e2eceff88260a223370ab9f6758d34817188
eslam <es-eslam.dyab2019@alexu.edu.eg>
2021-08-31 00:26:15 +0200

    final

--- main.cpp
+++ main.cpp
@@ -6,6 +6,76 @@ using namespace std;
 #include "MonsterHotel.h"
 
 
+void hum_reservation(int id, string name,int guest_num,int i,int *avalible_rooms,string monster_or_human);
+void mon_reservation(int id, string name,int guest_num,int i,int *avalible_rooms,string monster_or_human);
+void save_to_txt(int i,int id,string name,int reserved_rooms,int night_num,int total_cost,int room_num, int avalible_rooms,string monster_or_human);
+
+
+int main() {
+    int guest_num = 5;
+    int avalible_hum_rooms=5;
+    int avalible_mon_rooms=15;
+
+    // create an object guest array with size of guests number
+    Guest guests_arr [guest_num];
+
+    // looping through guests and take inputs
+    for(int i=0; i < guest_num; i++){
+        guests_arr[i].ask_new_or_old();
+        // TODO handling new guests (DONE)
+        if (guests_arr[i].new_or_old == "new")
+        {
+            guests_arr[i].create_account();
+            guests_arr[i].id = i+1;
+            guests_arr[i].display_id();
+
+            if(guests_arr[i].monster_or_human =="human") {
+                HumanHotel hotel_arr [guest_num];
+                hum_reservation(guests_arr[i].id,guests_arr[i].name,guest_num,i,&avalible_hum_rooms,guests_arr[i].monster_or_human);
+            }
+            else if (guests_arr[i].monster_or_human =="monster"){
+                MonsterHotel hotel_arr [guest_num];
+                mon_reservation(guests_arr[i].id,guests_arr[i].name,guest_num,i,&avalible_mon_rooms,guests_arr[i].monster_or_human);
+            }
+
+        }
+        // TODO handling old guests (DONE)
+        else if (guests_arr[i].new_or_old == "old")
+        {
+            int old_id = guests_arr[i].ask_for_id();
+            old_id = old_id - 1;
+
+            if(guests_arr[old_id].monster_or_human =="human") {
+                hum_reservation(guests_arr[old_id].id,guests_arr[old_id].name,guest_num,i,&avalible_hum_rooms,guests_arr[i].monster_or_human);
+            }
+
+            else if (guests_arr[old_id].monster_or_human =="monster"){
+                // realted to OLD Monsters problem, mon_reservation was hum_reservation
+                // now it's running as excpected
+                mon_reservation(guests_arr[old_id].id,guests_arr[old_id].name,guest_num,i,&avalible_mon_rooms,guests_arr[i].monster_or_human);
+            }
+        }
+
+        if (avalible_hum_rooms ==0 & avalible_mon_rooms !=0){
+            if(i+1 < guest_num){
+                cout << "no more human rooms avalible" << endl;
+            }
+        }
+        else if (avalible_hum_rooms !=0 & avalible_mon_rooms ==0){
+            if(i+1 < guest_num){
+                cout << "no more monster rooms avalible" << endl;
+            }
+        }
+        else if (avalible_hum_rooms ==0 & avalible_mon_rooms ==0){
+            cout << "Sorry no rooms avalible" << endl;
+            break;
+        }
+    }
+
+    return 0;
+}
+
+
 // TODO function that creates a text file to store data (DONE)
 //  right now it has a problem with OLD Monsters (SOLVED)
 //      it gets the occupied rooms (room_num - avalible_rooms)
@@ -150,66 +220,3 @@ void mon_reservation(int id, string name,int guest_num,int i,int *avalible_rooms
 }
 
 
-int main() {
-    int guest_num = 5;
-    int avalible_hum_rooms=5;
-    int avalible_mon_rooms=15;
-
-    // create an object guest array with size of guests number
-    Guest guests_arr [guest_num];
-
-    // looping through guests and take inputs
-    for(int i=0; i < guest_num; i++){
-        guests_arr[i].ask_new_or_old();
-        // TODO handling new guests (DONE)
-        if (guests_arr[i].new_or_old == "new")
-        {
-            guests_arr[i].create_account();
-            guests_arr[i].id = i+1;
-            guests_arr[i].display_id();
-
-            if(guests_arr[i].monster_or_human =="human") {
-                HumanHotel hotel_arr [guest_num];
-                hum_reservation(guests_arr[i].id,guests_arr[i].name,guest_num,i,&avalible_hum_rooms,guests_arr[i].monster_or_human);
-            }
-            else if (guests_arr[i].monster_or_human =="monster"){
-                MonsterHotel hotel_arr [guest_num];
-                mon_reservation(guests_arr[i].id,guests_arr[i].name,guest_num,i,&avalible_mon_rooms,guests_arr[i].monster_or_human);
-            }
-
-        }
-        // TODO handling old guests (DONE)
-        else if (guests_arr[i].new_or_old == "old")
-        {
-            int old_id = guests_arr[i].ask_for_id();
-            old_id = old_id - 1;
-
-            if(guests_arr[old_id].monster_or_human =="human") {
-                hum_reservation(guests_arr[old_id].id,guests_arr[old_id].name,guest_num,i,&avalible_hum_rooms,guests_arr[i].monster_or_human);
-            }
-
-            else if (guests_arr[old_id].monster_or_human =="monster"){
-                // realted to OLD Monsters problem, mon_reservation was hum_reservation
-                // now it's running as excpected
-                mon_reservation(guests_arr[old_id].id,guests_arr[old_id].name,guest_num,i,&avalible_mon_rooms,guests_arr[i].monster_or_human);
-            }
-        }
-
-        if (avalible_hum_rooms ==0 & avalible_mon_rooms !=0){
-            if(i+1 < guest_num){
-                cout << "no more human rooms avalible" << endl;
-            }
-        }
-        else if (avalible_hum_rooms !=0 & avalible_mon_rooms ==0){
-            if(i+1 < guest_num){
-                cout << "no more monster rooms avalible" << endl;
-            }
-        }
-        else if (avalible_hum_rooms ==0 & avalible_mon_rooms ==0){
-            cout << "Sorry no rooms avalible" << endl;
-            break;
-        }
-    }
-
-    return 0;
-}
